0d2f874e064694016a06fd2e46b00c47944cbb4d
JPEmery <clearkeyemery@gmail.com>
2018-08-02 20:16:04 -0500

    JP Emery: added functionality to adv

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -20,21 +20,61 @@ to north. The smell of gold permeates the air."""),
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
     'library': Room("library", """You have found a place for reading. Unfortunately In your adventures you never learned the Dewey Decimal System so finding books is very difficult""")
+
+'library': Room("library", """You have found a place for reading. Unfortunately In your adventures you never learned the Dewey Decimal System so finding books is very difficult""")
+    
+
 }
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
+
 room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
+room['foyer'].n_to = room['staircase']
+room['foyer'].w_to = room['livingroom']
+room['livingroom'].n_to = room['kitchen']
+room['kitchen'].e_to = room['breakfastnook']
+room['breakfastnook'].w_to = room['kitchen']
+
+room['staircase'].n_to = room['landing']
+room['foyer'].e_to = room['library']
+room['staircase'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
-room['overlook'].w_to = room['library']
-room['library'].e_to = room['overlook']
+
+
+#upstairs
+room['landing'].e_to = room['bedroom5']
+
+room['landing'].w_to = room['longhallway1']
+room['longhallway1'].w_to = room['longhallway2']
+room['longhallway1'].s_to = room['bedroom2']
+room['longhallway1'].n_to = room['bedroom1']
+room['longhallway2'].w_to = room['balcony']
+room['longhallway2'].n_to = room['bedroom3']
+room['longhallway2'].s_to = room['bedroom4']
+
+# room['overlook'].w_to = room['library']
+room['library'].key_to = room['secretroom']
+room['secretroom'].s_to = room['sprialstaircase']
+room['secretroom'].n_to = room['library']
+room['sprialstaircase'].n_to = room['secretroom']
+room['sprialstaircase'].s_to = room['largecolumedexpanse']
+
+room['largecolumedexpanse'].e_to = room['largecolumedexpanse2']
+room['largecolumedexpanse2'].e_to = room['largecolumedexpanse3']
+room['largecolumedexpanse2'].w_to = room['largecolumedexpanse']
+room['largecolumedexpanse3'].w_to = room['largecolumedexpanse2']
+room['Largecolumedexpanse'].s_to = room['sprialstaircase']
+
+room['Litlargecolumedexpanse'].e_to = room['Litlargecolumedexpanse2']
+room['Litlargecolumedexpanse2'].e_to = room['Largecolumedexpanse3']
+room['Litlargecolumedexpanse2'].w_to = room['Largecolumedexpanse']
+room['Litlargecolumedexpanse3'].w_to = room['Largecolumedexpanse2']
+room['Litlargecolumedexpanse'].s_to = room['sprialstaircase']
+
 
 #
 # Main
