cd55bf7e01f83d982f25f20c8dd90f08b0f678c2
Greg Knudsen <dev.artist15@gmail.com>
2018-08-08 16:10:11 -0400

    working on hangman

--- src/mini-challenge/hangman.py
+++ src/mini-challenge/hangman.py
@@ -6,6 +6,8 @@
 # it? Add a cheat code, more error handling, chose randomly
 # from a set of win/loss messages...basically, make it better!
 
+import random
+
 # Initial setup
 bodies = [ " ------\n |    |\n |    O\n |\n |\n |\n |\n |\n---", 
 " ------\n |    |\n |    O\n |    |\n |    |\n |\n |\n |\n---", 
@@ -15,18 +17,18 @@ bodies = [ " ------\n |    |\n |    O\n |\n |\n |\n |\n |\n---",
 " ------\n |    |\n |  O\n |   \|/\n |    |\n |   / \ \n |\n |\n---" ]
 strikes = 0
 words = [None]
-file = open("word.txt", "r")
+file = open("words.txt", "r")
 for line in file:
     words.append(line)
 file.close()
 targetWord = words[random.randint(0, 100)]
 lettersLeft = len(targetWord)-1
 length = len(targetWord)-1
+alphabet = [chr(65+x) for x in range(0, 26) ]
 curWord = "_" * length
-alphabet = [chr(65+x) for x in range(1, 26) ]
 
 # Draw body based on # of incorrect guesses
-def drawBody()
+def drawBody():
     print(bodies[strikes])
 
 # Replace blanks with correctly guessed letters
@@ -59,7 +61,7 @@ while strikes < 5 and lettersLeft > 0:
         fillLetters(letter)
     else:
         strikes += 1
-        print( strikes + " / 5 strikes" )
+        print( str(strikes) + " / 5 strikes" )
     printWord(curWord)
     drawBody()
     alphabet.remove(letter.upper())
