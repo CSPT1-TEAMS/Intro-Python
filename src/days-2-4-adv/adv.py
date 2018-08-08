7768b18159adc6feaa15545b6e2e0080ba98d11e
Andrew Jarrett <ahrjarrett@gmail.com>
2018-08-07 22:17:30 -0500

    created Player, Item, Room classes; next read commands from user

--- src/days-2-4-adv/adv.py
+++ src/days-2-4-adv/adv.py
@@ -1,24 +1,39 @@
 from room import Room
+from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
+    'library': Room('Library',
+                    'The universe (which others call the Library) is composed of an indefinite, perhaps infinite number of hexagonal galleries.',
+                    []),
+
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",
+                     []),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer",
+                     """Dim light filters in from the south. Dusty
+                     passages run north and east.""",
+                     []),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook",
+                     """A steep cliff appears before you, falling
+                     into the darkness. Ahead to the north, a light flickers in
+                     the distance, but there is no way across the chasm.""",
+                     []),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage",
+                     """The narrow passage bends here from west
+                     to north. The smell of gold permeates the air.""",
+                     []),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber",
+                     """You've found the long-lost treasure
+                     chamber! Sadly, it has already been completely emptied by
+                     earlier adventurers. The only exit is to the south.""",
+                     []),
 }
 
 
@@ -28,21 +43,42 @@ room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
 room['foyer'].e_to = room['narrow']
+room['foyer'].w_to = room['library']
 room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+room['library'].e_to = room['foyer']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+andrew = Player(
+    'Andrew',
+    room['outside'],
+    [Item(
+        'Old Notebook',
+        """You seem to be holding an old, water-stained notebook.
+        Some of the pages appear to be torn out, although you can almost make out
+        what was scribbled onto the last page... it looks like it might be someone's name."""
+    )]
+)
+
+cmd = ''
+
+
+def print_room_info():
+    # * Prints the current room name
+    print(andrew.room.name)
+    # * Prints the current description (the textwrap module might be useful here)
+    print(andrew.room.description)
+    # * Print all the items in the current room
+    andrew.room.print_items()
 
 # Write a loop that:
 #
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
--- src/days-2-4-adv/room.py
+++ src/days-2-4-adv/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description, items):
+        self.name = name
+        self.description = description
+        self.items = items
+    def print_items(self):
+        if len(self.items) == 0:
+            print("No items in the " + andrew.room.name)
+        else:
+            for item in self.items:
+                print('\t', item)
+
+            
